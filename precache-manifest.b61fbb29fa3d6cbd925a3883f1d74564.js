4e99fd192f551c1f6405b8c2e3b7c248365f5435
riflowth <riflowth@gmail.com>
2023-01-22 23:29:22 +0700

    build preview

---
+++ precache-manifest.b61fbb29fa3d6cbd925a3883f1d74564.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e13dc2d57a8a6ca9ed4f43aaf9d3e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2644653b9dd543c32027",
+    "url": "/static/css/2.929389fa.chunk.css"
+  },
+  {
+    "revision": "a8f7e340baae63bc7f1b",
+    "url": "/static/css/main.4c53984b.chunk.css"
+  },
+  {
+    "revision": "2644653b9dd543c32027",
+    "url": "/static/js/2.5c66d1ac.chunk.js"
+  },
+  {
+    "revision": "5a9275e59538a53c012e5a92e6269dcd",
+    "url": "/static/js/2.5c66d1ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8f7e340baae63bc7f1b",
+    "url": "/static/js/main.79934105.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "62baf0f264db531a9457567a97c2311d",
+    "url": "/static/media/Button_Frame.62baf0f2.png"
+  },
+  {
+    "revision": "a4c5e1e966947ea2a3c4c07a1f3b0650",
+    "url": "/static/media/CC_Button_middle.a4c5e1e9.png"
+  },
+  {
+    "revision": "db42d1c9224a813ecc545475eea18ead",
+    "url": "/static/media/General_BG_new.db42d1c9.png"
+  },
+  {
+    "revision": "e66ac1c726c034e0893ecde523c2cc5c",
+    "url": "/static/media/img8.e66ac1c7.png"
+  },
+  {
+    "revision": "028337253a13eb43df278ec7aa660037",
+    "url": "/static/media/logo-big.02833725.png"
+  }
+]);
